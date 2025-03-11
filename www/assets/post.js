e3dae9914792305852ccbe665561a3692615acfa
Luke Dashjr <luke-jr+git@utopios.org>
2025-03-11 20:09:39 +0000

    api: Handle error responses in sendPostRequest

--- www/assets/post.js
+++ www/assets/post.js
@@ -1,10 +1,16 @@
-function sendPostRequest(url, data) {
+async function sendPostRequest(url, data) {
 	data.csrf = '%s';
-	fetch(url, {
+	var r = await fetch(url, {
 		method: 'POST',
 		headers: {
 			'Content-Type':'application/json'
 		},
 		body: JSON.stringify(data)
 	});
+	if (r.ok) return;
+	if (r.status == 401) {
+		alert('This action requires admin access.');
+	} else {
+		alert('Error ' + r.status);
+	}
 }
