6b7d7dbd39584c3974e2c6b7c65fcef756e32141
Luke Dashjr <luke-jr+git@utopios.org>
2025-07-16 20:49:57 +0000

    Update copyright years

--- src/datum_gateway.c
+++ src/datum_gateway.c
@@ -10,7 +10,7 @@
  *
  * ---
  *
- * Copyright (c) 2024 Bitcoin Ocean, LLC & Jason Hughes
+ * Copyright (c) 2024-2025 Bitcoin Ocean, LLC & Jason Hughes
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
