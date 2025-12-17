a905e7fa625d9fb42cb9b2e7ddfb60dd9e3c2b22
Luke Dashjr <luke-jr+git@utopios.org>
2025-12-17 16:02:59 +0000

    QA: utils: Exercise hex2bin_uchar, uchar_to_hex, and hex_to_bin[_le]

--- src/datum_utils_tests.c
+++ src/datum_utils_tests.c
@@ -34,11 +34,68 @@
  */
 
 #include <stddef.h>
+#include <stdio.h>
 #include <string.h>
+#include <strings.h>
 
 #include "datum_utils.h"
 
-void datum_utils_tests(void) {
+void datum_utils_tests_hex_to_bin(const uint8_t c, char * const x, const char * const fmt) {
+	unsigned char b[3];
+	datum_test(2 == snprintf(&x[2], 3, fmt, c));
+	datum_test(c == hex2bin_uchar(&x[2]));
+	memcpy(x, "00", 2);
+	b[2] = 0x0e;
+	hex_to_bin(x, b);
+	datum_test(b[0] == 0);
+	datum_test(b[1] == c);
+	datum_test(b[2] == 0x0e);
+	b[2] = 0x0e;
+	hex_to_bin_le(x, b);
+	datum_test(b[0] == c);
+	datum_test(b[1] == 0);
+	datum_test(b[2] == 0x0e);
+}
+
+void datum_utils_tests_hex(void) {
+	char x[6], x2[6];
+	strcpy(&x[2], "00");
+	for (unsigned int c = 0; ; ++c) {
+		datum_utils_tests_hex_to_bin(c, &x2[1], "%2.2X");
+		datum_test(strcasecmp(&x[2], &x2[3]) == 0);
+		datum_utils_tests_hex_to_bin(c, x2, "%2.2X");
+		datum_utils_tests_hex_to_bin(c, &x[1], "%2.2x");
+		datum_test(strcasecmp(&x[3], &x2[2]) == 0);
+		datum_utils_tests_hex_to_bin(c, x, "%2.2x");
+		datum_test(strcasecmp(&x[2], &x2[2]) == 0);
+		
+		x2[2] = 0x0e;
+		uchar_to_hex(x2, c);
+		datum_test(memcmp(&x[2], x2, 2) == 0);
+		datum_test(x2[2] == 0x0e);
+		x2[3] = 0x0e;
+		uchar_to_hex(&x2[1], c);
+		datum_test(memcmp(&x[2], &x2[1], 2) == 0);
+		datum_test(x2[3] == 0x0e);
+		
+		if (c == 255) break;
+		
+		if (x[3] == 'f') {
+			x[3] = '0';
+			if (x[2] == '9') {
+				x[2] = 'a';
+			} else {
+				++x[2];
+			}
+		} else if (x[3] == '9') {
+			x[3] = 'a';
+		} else {
+			++x[3];
+		}
+	}
+}
+
+void datum_utils_tests_secure_strequals(void) {
 	const char * const secret = "abc";
 	const size_t secret_len = strlen(secret);
 	
@@ -57,4 +114,9 @@ void datum_utils_tests(void) {
 	/* zero-length secret matches only on empty guess, and doesn't dereference NULL */
 	datum_test(!datum_secure_strequals(NULL, 0, "anything"));
 	datum_test(datum_secure_strequals(NULL, 0, ""));
+}
+
+void datum_utils_tests(void) {
+	datum_utils_tests_hex();
+	datum_utils_tests_secure_strequals();
 }
